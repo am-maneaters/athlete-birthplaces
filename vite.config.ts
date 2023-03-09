76d8efec37e0dcf97e5d1675c50f2451ed36efc8
Sam Matenaer <smatenaer@esri.com>
2023-03-08 23:07:59 -0800

    new output file names

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/athlete-birthplaces/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      },
+    },
+  },
 });
